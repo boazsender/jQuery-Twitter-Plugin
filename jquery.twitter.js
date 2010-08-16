921e1db05f9de9c3f36d009eba38257f3955a12e
rwldrn <waldron.rick@gmail.com>
2010-08-15 20:00:24 -0400

    additional unit tests

--- jquery.twitter.js
+++ jquery.twitter.js
@@ -20,7 +20,7 @@
   var opts = {
         limit        : 7,     // Number of tweets to get <-- not in twitter api, maps to and superseeds rpp (results per page)
         exclusions   : '',    // Strings to exclude <-- not in twitter api, done in plugin
-        notFoundText : '',    // Text to display if no results are found <-- not in twitter api, done in plugin
+        notFoundText : 'No results found on twitter',    // Text to display if no results are found <-- not in twitter api, done in plugin
         replies      : true,  // Include replies? <-- not in twitter api, done in plugin
         retweets     : true,  // Include replies? <-- not in twitter api, done in plugin
         ands    : '', // All of these words  
@@ -40,7 +40,7 @@
         tude    : '', // Attitude: '?' or ':)' or ':)'
         filter  : '', // Containing: 'links'
         include : '', // Include retweet?: 'retweets'
-        rpp     : 2,
+        rpp     : 5,
         q       : ''   // Results per page
       },
       linkify   = (function(){var k="[a-z\\d.-]+://",h="(?:(?:[0-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])\\.){3}(?:[0-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])",c="(?:(?:[^\\s!@#$%^&*()_=+[\\]{}\\\\|;:'\",.<>/?]+)\\.)+",n="(?:ac|ad|aero|ae|af|ag|ai|al|am|an|ao|aq|arpa|ar|asia|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|biz|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|cat|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|coop|com|co|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|info|int|in|io|iq|ir|is|it|je|jm|jobs|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mobi|mo|mp|mq|mr|ms|mt|museum|mu|mv|mw|mx|my|mz|name|na|nc|net|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pro|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|travel|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|xn--0zwm56d|xn--11b5bs3a9aj6g|xn--80akhbyknj4f|xn--9t4b11yi5a|xn--deba0ad|xn--g6w251d|xn--hgbk6aj7f53bba|xn--hlcj6aya9esc7a|xn--jxalpdlp|xn--kgbechtv|xn--zckzah|ye|yt|yu|za|zm|zw)",f="(?:"+c+n+"|"+h+")",o="(?:[;/][^#?<>\\s]*)?",e="(?:\\?[^#<>\\s]*)?(?:#[^<>\\s]*)?",d="\\b"+k+"[^<>\\s]+",a="\\b"+f+o+e+"(?!\\w)",m="mailto:",j="(?:"+m+")?[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"+f+e+"(?!\\w)",l=new RegExp("(?:"+d+"|"+a+"|"+j+")","ig"),g=new RegExp("^"+k,"i"),b={"'":"`",">":"<",")":"(","]":"[","}":"{","B;":"B+","b:":"b9"},i={callback:function(q,p){return p?'<a href="'+p+'" title="'+p+'">'+q+"</a>":q},punct_regexp:/(?:[!?.,:;'"]|(?:&|&amp;)(?:lt|gt|quot|apos|raquo|laquo|rsaquo|lsaquo);)$/};return function(u,z){z=z||{};var w,v,A,p,x="",t=[],s,E,C,y,q,D,B,r;for(v in i){if(z[v]===undefined){z[v]=i[v]}}while(w=l.exec(u)){A=w[0];E=l.lastIndex;C=E-A.length;if(/[\/:]/.test(u.charAt(C-1))){continue}do{y=A;r=A.substr(-1);B=b[r];if(B){q=A.match(new RegExp("\\"+B+"(?!$)","g"));D=A.match(new RegExp("\\"+r,"g"));if((q?q.length:0)<(D?D.length:0)){A=A.substr(0,A.length-1);E--}}if(z.punct_regexp){A=A.replace(z.punct_regexp,function(F){E-=F.length;return""})}}while(A.length&&A!==y);p=A;if(!g.test(p)){p=(p.indexOf("@")!==-1?(!p.indexOf(m)?"":m):!p.indexOf("irc.")?"irc://":!p.indexOf("ftp.")?"ftp://":"http://")+p}if(s!=C){t.push([u.slice(s,C)]);s=E}t.push([A,p])}t.push([u.substr(s)]);for(v=0;v<t.length;v++){x+=z.callback.apply(window,t[v])}return x||u}})(),
@@ -56,6 +56,12 @@
       };
       
   $.fn.twitter = function (options) {
+    
+    if ( !options ) {
+      return this;
+    }
+  
+  
     // Set a temporary default query object
     var query,
         exclusionsExp = new RegExp(false);
@@ -69,25 +75,17 @@
       // If a limit is set, add it to the query object
       options.rpp = options.limit ? options.limit : options.rpp;
 
-      // If there are exclusions, multiply the results to ask for from twitter by ten
-      // We need to do this so that we have some meat to work with if the exclusions are common
-      options.rpp = (options.exclusions && options.exclusions.length) ? (options.rpp * 10) : options.rpp;
-
       // If there are exlusions, turn them into a regex to use later
       exclusionsStr = options.exclusions ? options.exclusions.replace(' ', '|') : false;
       exclusionsExp = exclusionsStr ? new RegExp(exclusionsStr) : false;
-      
-      // If text to display if no results are found is set, use it, otherwise, set it
-      notFoundText = options.notFoundText ? options.notFoundText : 'No results found on twitter';
+
 
       // Merge temp query it with the default options
       query = $.extend({}, opts, options);
 
-      // Delete the properies from the options object that we're not sendiing to twitter search api
-      delete query.limit;
-      delete query.exclusions;
-      delete query.notFoundText;
-
+      // If there are exclusions, multiply the results to ask for from twitter by ten
+      // We need to do this so that we have some meat to work with if the exclusions are common
+      query.rpp = query.exclusions || !query.replies || !query.retweets  ? (options.rpp * 10) : options.rpp;      
     }
     
     return this.each(function () {
@@ -101,7 +99,8 @@
       $.getJSON('http://search.twitter.com/search.json?'+$.param(query)+'&callback=?', function(tweets){ 
 
         //Create and cache a new UL
-        var $tweets = $('<ul>');
+        var $tweets   = $('<ul>'), 
+            limitInt  = 0;
         
         // If there are results to work with 
         if (tweets.results && tweets.results.length) {
@@ -110,11 +109,11 @@
           for(var i in tweets.results){
 
             // Cache tweet content
-            var tweet      = tweets.results[i],
+            var tweet         = tweets.results[i],
                 // Set a variable to determine weather replies are set to false, and if so, weather the tweet starts with a reply
-                allowReply   = !query.replies && tweet.to_user_id ? false : true,
+                allowReply    = !query.replies && tweet.to_user_id ? false : true,
                 // Set a variable to determine weather retweets are set to false, and if so, weather the tweet starts with a retweet
-                allowRetweet = !query.retweets && tweet.text.slice(0,2) == 'RT' ? false : true;
+                allowRetweet  = !query.retweets && tweet.text.slice(0,2) == 'RT' ? false : true;
             
             // Only proceed if allow reply is false
             if (!allowReply) {
@@ -147,6 +146,12 @@
             }))
             // Append tweet to the $tweets ul
             .appendTo($tweets);
+            
+            limitInt++;            
+            
+            if ( limitInt === options.rpp ) {
+              break;
+            }
           }
           
           // Append the $tweets to the DOM
@@ -157,7 +162,7 @@
           // Update the DOM to reflect that no results were found
           $this.html($('<h3/>', {
             className: 'twitter-notFound',
-            text: notFoundText
+            text: query.notFoundText
           }));
         }
       });
--- unit/unit.js
+++ unit/unit.js
@@ -1,14 +1,21 @@
 $(function(){
+  
+  var flags = [];
+
+
   $('#testlist1').twitter({
     from:'mediatemple',
     rpp: 2
   });
+  
   $('#testlist2').twitter({
     from:'whatever', 
     limit:0, 
     notFoundText: 'Whoops, no results'
   });
-  $('#testlist3').twitter('whatever');
+  
+  $('#testlist3').twitter('ikjn gt oidfbgjldkfobnidfjoh;bikdjfkbhjfldldfj');
+  
   $('#testlist4').twitter({
           ands    : 'I',           // All of these words  
           phrase  : '',            // This exact phrase 
@@ -27,33 +34,101 @@ $(function(){
           tude    : '',              // Attitude: '?' or ':)' or ':)'
           filter  : '',              // Containing: 'links'
           include : '',              // Include retweet?: 'retweets'
-          rpp     : 3                // Results per page
+          rpp     : 4                // Results per page
         });
 
+
+  $('#testlist5').twitter({
+          from    : 'rwaldron',              // From this person  
+          replies:  false,
+          retweets: false,
+          rpp     : 4                // Results per page
+        });
   
   module("Basic Tests");
   test("Test the signatures", function() {
+    
     ok($.isFunction($.fn.twitter), "$.fn.twitter exists and is a function" );
     
     //console.log(typeof $.fn.twitter());
-    equals( 'object', (typeof $.fn.twitter({})), "$.fn.twitter() returns and object" );
-    
-    equals( 'undefined', (typeof $.fn.twitter()), "$.fn.twitter() returns and object" );
+    equals( 'object', (typeof $.fn.twitter({})), "$.fn.twitter({}) returns and object" );
+    equals( 'string', (typeof $.fn.twitter({}).selector), "$.fn.twitter({}) returns and object" );
+
+    equals( 'object', (typeof $.fn.twitter('foo')), "$.fn.twitter('foo') returns and object" );
+    equals( 'string', (typeof $.fn.twitter('foo').selector), "$.fn.twitter('foo') returns and object" );
+
+    equals( 'object', (typeof $.fn.twitter()), "$.fn.twitter() returns and object" );
+    equals( 'string', (typeof $.fn.twitter().selector), "$.fn.twitter() returns and object" );
+
+    equals( 'object', (typeof $.fn.twitter(null)), "$.fn.twitter(null) returns and object" );
+    equals( 'string', (typeof $.fn.twitter(null).selector), "$.fn.twitter(null) returns and object" );    
+
+    equals( 'object', (typeof $.fn.twitter(undefined)), "$.fn.twitter(null) returns and object" );
+    equals( 'string', (typeof $.fn.twitter(undefined).selector), "$.fn.twitter(null) returns and object" );    
+
     
   });
   test("Test twitter list that is built", function() {
-    equals( 1, $('#testlist1').children().length, "The element with .twitter() called on it should have one child" );
-    equals( 2, $('#testlist1').children().children.length, "The twitter list should have two children" );
+  
+  
+    equals( 1, $('#testlist1').children().length, "Any element with $.fn.twitter() should have exactly one child" );
+    equals( 1, $('#testlist2').children().length, "Any element with $.fn.twitter() should have exactly one child" );
+    equals( 1, $('#testlist3').children().length, "Any element with $.fn.twitter() should have exactly one child" );    
+    equals( 1, $('#testlist4').children().length, "Any element with $.fn.twitter() should have exactly one child" );    
+    
+    
+    equals( 2, $('#testlist1').children().children().length, "The twitter list should have 2 children" );
+    equals( 4, $('#testlist4').children().children().length, "The twitter list should have 4 children" );    
+    
     equals( '@', $('#testlist1').text().match(/@/), "The twitter lis should have at least one @ symbol in it" );
+    
     equals( 'Whoops, no results', $('#testlist2').text(), "The failed search should say 'Whoops, no results'" );
+    
     equals( 'No results found on twitter', $('#testlist3').text(), "The failed search should default to 'No results found on twitter'" );
+    
     ok($('#testlist1').children().find('a').find('img').length, "Make sure the user avatar is present and inside of an anchor" );
+    
     ok($('#testlist1').children().find('span').length, "Make sure the tweet is there" );
+    
   });
   test("Test a few of the cases for the object style signature", function() {
+    
     equals( null, $('#testlist4').text().match(/dirty/), "dirty should not show up" );
+    
     equals( 'I', $('#testlist4').text().match(/I/), "dirty should not show up" );
+    
     equals( 'omg', $('#testlist4').text().match(/omg/), "dirty should not show up" );
+    
+    
+  });
+
+  test("Test no replies, no retweets", function() {
+    
+    var $li = $('#testlist5 ul li'), 
+        _li = [],
+        _rp = [],
+        _rt = [];
+    
+    
+    for ( var i = 0; i < $li.length; i++ ) {
+      _li.push( $.trim($( $li[i] ).text().split(':')[1]) );
+      
+      if ( _li[i].indexOf('RT') === 0 ) {
+        _rt.push(true);
+      }
+
+      if ( _li[i].indexOf('@') === 0 ) {
+        _rp.push(true);
+      }
+    }
+    
+    equals( 4, _li.length, 'There are four tweets' );
+    equals( 0, _rt.length, 'There are no Retweets' );
+    equals( 0, _rp.length, 'There are no Replies' );
+    
+    
+    
+    
   });
 
 });
